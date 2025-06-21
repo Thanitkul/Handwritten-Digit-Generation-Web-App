5e101f5df6d6017146a39ed7df86c1b4086a2fde
Phasit Thanitkul_Kane <kanephasit@gmail.com>
2025-06-21 23:54:36 +0700

    20 epoch final

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def load_generator():
     latent_dim = 100
     num_classes = 10
     model = Generator(latent_dim, num_classes)
-    model.load_state_dict(torch.load("models/generator_epoch20.pth", map_location="cpu"))
+    model.load_state_dict(torch.load("models/generator.pth", map_location="cpu"))
     model.eval()
     return model
 
